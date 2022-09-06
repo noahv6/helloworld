7fd91a34fad7a43c06c15cd17a9518d1e31ba67c
noahv6 <111916853+noahv6@users.noreply.github.com>
2022-09-06 15:58:13 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace deliverable_1 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string stg = "hello, World! (2)";
+            Console.WriteLine(stg);
+
+            Console.WriteLine("Enter username: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter user's age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Username is: " + username + " and the age is " + age);
+
+
+        }
+    }
+}
